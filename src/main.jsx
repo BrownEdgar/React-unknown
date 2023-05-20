6225f17778839189b666151b42e6a66281207386
Karen <karentumanyan0@gmail.com>
2023-05-20 22:28:42 +0400

    create data in objects and show it in main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Users/Edgar/workWIthImage/App'
-
+// import App from './Users/Karen/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

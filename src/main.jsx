ee7eba6a5b2fc8e0735f2eafe800c30d8aed757b
Hovo Tumanyan <hovotumanyanyana@gmail.com>
2023-05-21 16:11:10 +0400

    manual data

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Users/Edgar/workWIthImage/App'
 
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

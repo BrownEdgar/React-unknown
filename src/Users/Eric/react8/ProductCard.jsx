5dc4a74f88c9b1e21f4d9209aedc7ea2b2c69a50
EroBalasanyan <ericbalasan@gmail.com>
2023-05-22 17:37:31 +0400

    react8

---
+++ src/Users/Eric/react8/ProductCard.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+export default function ProductCard({title, description, image}){
+    return(
+        <div className="productCard">
+            <img src={image} alt={title}/>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+ProductCard.PropTypes = {
+    products: PropTypes.arrayOf(
+        PropTypes.exact(
+            {
+                id: PropTypes.number.isRequired,
+                title: PropTypes.string.isRequired,
+                description: PropTypes.string.isRequired,
+                // image: PropTypes.image.isRequired
+            }
+        )
+    )
+}
---
+++ src/Users/Eric/react8/data.js
@@ -0,0 +1,25 @@
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg'
+
+const products = [
+    {
+        id: 1,
+        title: "Better Chemistry",
+        description: "We're committed to making products in ways that protect consumers, wokers and the environment.",
+        image: image1
+    },
+    {
+        id: 2,
+        title: "Better Practices",
+        description: "We seek to work with vendors and suppliers who are commited to high standards and continuous improvement.",
+        image: image2
+    },
+    {
+        id: 3,
+        title: "Industry Collabration",
+        description: "We work across our industry to improvement and chemistry standards throughout global supply chains",
+        image: image3
+    }
+]
+export default products

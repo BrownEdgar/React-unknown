7ee9b108c6a91fbc96f1b1b969d657d5939397fb
Hovo Tumanyan <hovotumanyanyana@gmail.com>
2023-05-24 17:17:41 +0400

    modal,modul_css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Users/Edgar/workWIthImage/App'
-
-
-
-
+import App from './Users/Edgar/workWithImage/App'
+// import App from './Users/Hovo/PropsChild/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

ac27f637d333c74c498d380f1e35276cd05bab35
Karen <karentumanyan0@gmail.com>
2023-06-19 03:30:19 +0400

    message

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './Users/Edgar/hooks/UseReducer/App'
-
+// import App from './Users/Edgar/hooks/UseReducer/App'
+import App from './Users/Karen/Slide/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

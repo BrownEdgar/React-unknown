ccc976b04580299d92fed20a3fbe195fb3b7983c
Hovo Tumanyan <hovotumanyanyana@gmail.com>
2023-06-18 14:37:09 +0400

    slider

--- src/Users/Hovo/Reducer/App.jsx
+++ src/Users/Hovo/Reducer/App.jsx
@@ -1,15 +1,10 @@
 import { useReducer } from "react";
 import reducer, { initialState } from './reducer';
-import { ADD_SHUFFLE, ADD_POSTS, ADD_DEVELOPER } from "./actionTypes";
+import { ADD_SHUFFLE, ADD_POSTS, DELETE_POST, ADD_ID, ADD_DEVELOPER } from "./actionTypes";
 import axios from "axios";
-import * as yup from 'yup';
-import { Formik, Form, Field } from "formik";
 import { BsFillTrashFill } from "react-icons/bs";
+import MyForm from "./MyForm";
 import './App.scss';
-
-const validationSchema = yup.object({
-  name: yup.string().required(),
-});
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -30,55 +25,37 @@ export default function App() {
   }
 
   const handleDelete = (id) => {
-		const filtered = state.posts.filter(elem => elem.id !== id);
-		dispatch ({ type: ADD_POSTS, payload: filtered });
+		dispatch ({ type: DELETE_POST,payload:id});
 	}
 
   const handleChange = () => {
-    const changeId = state.posts.map((post, index) => ({...post,id: state.arr[index]}));
-      dispatch({ type: ADD_POSTS, payload: changeId});
+      dispatch({ type: ADD_ID});
   };
 
   return (
     <div className="container">
-  <h1 className="actions">Actions: {state.actions}</h1>
-  <Formik
-    initialValues={{ name: '' }}
-    onSubmit={handleSubmit}
-    validationSchema={validationSchema}
-  >
-    <Form>
-      <Field
-        type='text'
-        id='name'
-        name='name'
-        placeholder='Enter your name'
-        className="input-field"
-      />
-      <input type="submit" value="Submit" className="submit-btn" />
-    </Form>
-  </Formik>
-  <h1 className="developers">Developers: {state.developers + ''}</h1>
-  <h1 className="array">Array: {JSON.stringify(state.arr)}</h1>
-  <div className="btn-group">
-    <button onClick={handlePosts} >Load Posts</button>
-    <button onClick={handleShuffle} >Shuffle</button>
-    <button onClick={handleChange} >Change ID</button>
-  </div>
-  
-  <div className="items">
-    {state.posts.map(post => (
-      <div key={post.id} className="post" >
-        <p>
-          ID: {post.id} 
-          <BsFillTrashFill onClick={() => handleDelete(post.id)} className="delete-icon" />
-        </p>
-        <h3>Title: {post.title}</h3>
-        <p>Body: {post.body}</p>
-        
+      <h1 className="actions">Actions: {state.actions}</h1>
+      <MyForm handleSubmit={handleSubmit}/>
+      <h1 className="developers">Developers: {state.developers + ''}</h1>
+      <h1 className="array">Array: {JSON.stringify(state.arr)}</h1>
+      <div className="btn-group">
+        <button onClick={handlePosts} >Load Posts</button>
+        <button onClick={handleShuffle} >Shuffle</button>
+        <button onClick={handleChange} >Change ID</button>
       </div>
-    ))}
-  </div>
+      
+      <div className="items">
+        {state.posts.map(post => (
+          <div key={post.id} className="post" >
+            <p>
+              ID: {post.id} 
+              <BsFillTrashFill onClick={() => handleDelete(post.id)} className="delete-icon" />
+            </p>
+            <h3>Title: {post.title}</h3>
+            <p>Body: {post.body}</p>
+          </div>
+        ))}
+      </div>
 </div>
 
   );
--- src/Users/Hovo/Reducer/reducer.js
+++ src/Users/Hovo/Reducer/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_SHUFFLE, ADD_POSTS, ADD_DEVELOPER } from "./actionTypes";
+import { ADD_SHUFFLE, ADD_POSTS, ADD_ID, DELETE_POST, ADD_DEVELOPER } from "./actionTypes";
 
 export const initialState = {
     actions: 0,
@@ -7,9 +7,9 @@ export const initialState = {
     arr: [154, 42, 1, 87, 695, 36, 2, 10, 39, 9],
 };
 
-  
   export default function reducer(state = initialState, action) {
     switch (action.type) {
+      
       case ADD_POSTS:return  {
           ...state,
           posts: action.payload,
@@ -17,6 +17,8 @@ export const initialState = {
         };
         
       case ADD_SHUFFLE: return arrShuffle(state);
+      case ADD_ID: return newId(state);
+      case DELETE_POST: return deletePost(state, action.payload);
         
       case ADD_DEVELOPER:return {
           ...state, 
@@ -38,3 +40,21 @@ export const initialState = {
     const arr = state.arr.sort(() => Math.random() - 0.5);
     return { ...state, arr, actions: state.actions + 1 };
   };
+
+  const newId = (state) => {
+    const changeId = state.posts.map((post, index) => ({...post,id: state.arr[index]}));
+      return {
+        ...state,
+        actions: state.actions + 1,
+        posts: changeId,
+    };
+  }
+
+  const deletePost = (state,id) => {
+    const filtered = state.posts.filter(elem => elem.id !== id);
+    return {
+      ...state,
+      posts: filtered,
+      actions: state.actions + 1
+    }
+  }
